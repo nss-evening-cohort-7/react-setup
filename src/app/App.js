1a959846fd9d0ba6f6d0b70f60d2bcd9acd80a81
Zoe Ames <zoeames@gmail.com>
2018-06-25 20:27:53 -0500

    added eslint

--- src/app/App.js
+++ src/app/App.js
@@ -3,7 +3,7 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends React.Component {
-  render() {
+  render () {
     return (
       <div className="App">
         <header className="App-header">
